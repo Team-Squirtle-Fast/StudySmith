4fb7e04cecf512557d4dec4877e14dff080b9b80
shelbycarlin <shelbycarlin13@gmail.com>
2022-08-09 12:01:47 -0700

    removed unessecary double quotes from database and queries

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,19 @@
+//require in needed packages
 const express = require('express');
+//declare and intialize the express router 
 const router = express.Router();
+//require in the middleware functions
+const { createAccount, login, loginSucess } = require('../controller/controller.js');
 
-const controller = require('../controller/controller.js');
-
-
-router.post('/signup', controller.createAccount, (req, res) => {
+//route for post request to signup/create an account
+router.post('/signup', createAccount, (req, res) => {
     return res.status(200).json(res.locals.account);
 });
+
+//route to login in and fetch the data upon login
+router.get('/login', login, loginSucess, (req,res) => {
+    return res.status(200).json()
+})
 
 
 
