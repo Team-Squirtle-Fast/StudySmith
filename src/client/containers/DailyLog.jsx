2064be54a4407e26c98c70fc4b76d5f45d68e355
Allison Roderiques <aeroderiquees@gmail.com>
2022-08-09 22:16:35 -0500

    Daily Log

--- src/client/containers/DailyLog.jsx
+++ src/client/containers/DailyLog.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux"
+
+import { dailyLog } from "../actions/actions";
+
+/*
+res is going to be:
+{
+    title
+    log_body
+    log_date
+}
+*/
+
 
 const DailyLog = (props) => {
-    fetch('/api/log/:')
-      .then((res) => res.json())
-      .then((res) => {
+    // const dispatch = useDispatch();
 
-      })
+    // dispatch()
+    // fetch(`api/log/${username}`)
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //       for (let key in res) {
+
+    //       }
+    //   })
     return(
         <div>
             Daily Log;
